972ab9551cd07f6986cc3952626362f6aa7afb9c
Musa-Sina-Ertugrul <yutubems@gmail.com>
2024-08-28 09:45:40 +0300

    Solved ptr bug

--- train_app_model/gym_env/utils/scheduler.py
+++ train_app_model/gym_env/utils/scheduler.py
@@ -21,16 +21,22 @@ class Scheduler:
         self.__value = self.__decay_func()
         self.__current_epoch += 1
 
+    def __float__(self):
+        return self.value
 
 def linear_decay(end_iter: int) -> Callable:
     last_epoch = end_iter
-
+    base_value = None
     def inner_linear_decay(self: Scheduler) -> None:
+        nonlocal base_value
+        nonlocal last_epoch
+        if base_value is None:
+            base_value = self.value
         if last_epoch < self.current_epoch:
             raise RuntimeError(
                 f"last epoch: {last_epoch}, current epoch {self.current_epoch}.\
                 end_iter passed line. Raise end_iter"
             ) from None
-        return self.value - self.current_epoch * self.value / last_epoch
+        return self.value - self.current_epoch * base_value / last_epoch
 
     return inner_linear_decay
