472670fc18b03294f8c90f79a97881bec6c0553b
Musa-Sina-Ertugrul <yutubems@gmail.com>
2024-08-28 09:45:19 +0300

    Refactored

--- train_app_model/gym_env/main_env.py
+++ train_app_model/gym_env/main_env.py
@@ -4,20 +4,24 @@ import numpy as np
 import pandas as pd
 from itertools import count
 from gym_env.data_structures import Node, NodeType, DrawableNode
-from main_env_pygame import MainEnv
+from .main_env_pygame import MainEnv
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 class FintechAppDRLEnv(gym.Env):
 
     metadata = {"render_modes": ["human"], "render_fps": 60}
+    
+    font: pg.font.Font = pg.font.SysFont(pg.font.get_fonts()[0], 44)
 
     def __init__(self, memory_len: int):
         super().__init__()
         pg.init()
+        self.__true_finished_count = 0
         self.__screen = pg.display.set_mode((1280, 720))
         self.__clock = pg.time.Clock()
         self.__memory_len = memory_len
-        self.__env_map = self.__create_agent_map()
+        self._env_map = self.__create_agent_map()
         self.__current_node: Node = None
         self.__past_states = []
 
@@ -34,7 +38,8 @@ class FintechAppDRLEnv(gym.Env):
         df = df.drop("enrolled_date", axis=1)
         df["numscreens"] = df["numscreens"].astype(int)
         df["liked"] = df["liked"].astype(int)
-        df = df[df["numscreens"] == 65]
+        df = df[df["numscreens"] > 10]
+        df = df[df["numscreens"] < 50]
         df_liked = df[df["liked"] == 1]
         df_not_liked = df[df["liked"] == 0]
         screens_liked = df_liked["screen_list"].to_list()
@@ -53,59 +58,89 @@ class FintechAppDRLEnv(gym.Env):
             unique_screens.extend(np.unique(separated_user_screens))
         unique_screens_number_map = {}
         number_unique_screen_map = {}
-        for i, screen in zip(count(1), unique_screens):
-            unique_screens_number_map[screen] = i
+        unique_screens = list(CountVectorizer().fit(unique_screens).vocabulary_)
+        for i, screen in zip(count(2), unique_screens):
+            unique_screens_number_map[screen] = i 
             number_unique_screen_map[i] = screen
+        unique_screens_number_map["pad_screen"] = 0
+        number_unique_screen_map[0] = "pad_screen"
+        unique_screens_number_map["custom_screen"] = 1
+        number_unique_screen_map[1] = "custom_screen"
         self.__number_unique_screen_map = number_unique_screen_map
         self.__unique_screens_number_map = unique_screens_number_map
+        keys_set = set(unique_screens)
+        for i,user_screens in enumerate(separated_screens_liked.copy()):
+            for j,screen in enumerate(user_screens):
+                if screen not in keys_set:
+                    separated_screens_liked[i][j] = "custom_screen"
+        for i,user_screens in enumerate(separated_screens_not_liked.copy()):
+            for j,screen in enumerate(user_screens):
+                if screen not in keys_set:
+                    separated_screens_not_liked[i][j] = "custom_screen"
+        results = []
+        for user_screens in separated_screens_liked:
+            past_screen = ""
+            squeezed_screens = []
+            for screen in user_screens:
+                if past_screen != screen:
+                    squeezed_screens.append(screen)
+                    past_screen = screen
+            results.append(squeezed_screens)
+        separated_screens_liked = results
+        for user_screens in separated_screens_not_liked:
+            past_screen = ""
+            squeezed_screens = []
+            for screen in user_screens:
+                if past_screen != screen:
+                    squeezed_screens.append(screen)
+                    past_screen = screen
+            results.append(squeezed_screens)
+        separated_screens_not_liked = results
         env_map = {}
-        node_count = 0
         for user_screens in separated_screens_liked:
             node_ptr = None
+            past_node_ptr = None
             for i, screen in enumerate(user_screens):
                 if i == 0 and hasattr(env_map, screen):
-                    node_ptr = Node(
-                        screen,
-                        unique_screens_number_map[screen],
-                        NodeType.TRUE,
-                        node_count,
-                    )
-                    env_map[screen].add_child(node_ptr)
+                    node_ptr = env_map[screen]
                 elif i == 0 and not hasattr(env_map, screen):
                     node_ptr = Node(
                         screen,
-                        unique_screens_number_map[screen],
+                        -(len(user_screens)-i)/len(user_screens),
                         NodeType.TRUE,
-                        node_count,
+                        unique_screens_number_map[screen],i ==(len(user_screens)-1)
                     )
                     env_map[screen] = node_ptr
                 else:
                     new_node = Node(
                         screen,
-                        unique_screens_number_map[screen],
+                        -(len(user_screens)-i)/len(user_screens),
                         NodeType.TRUE,
-                        node_count,
+                        unique_screens_number_map[screen],i ==(len(user_screens)-1)
                     )
+                    past_node_ptr = node_ptr
                     node_ptr = node_ptr.add_child(new_node)
-                node_count += 1
-
-        separated_screens_not_liked = []
+                    node_ptr.add_child(past_node_ptr)
 
         for user_screens in separated_screens_not_liked:
+            past_node_ptr = None
             node_ptr = None
             for i, screen in enumerate(user_screens):
                 if i == 0 and hasattr(env_map, screen):
                     node_ptr = env_map[screen]
                 elif i == 0 and not hasattr(env_map, screen):
-                    break
-                else:
+                    node_name = np.random.choice(list(env_map.keys()))
+                    node_ptr = env_map[node_name]
+                else:                        
                     new_node = Node(
                         screen,
-                        unique_screens_number_map[screen],
+                        -1,
                         NodeType.WRONG,
-                        node_count,
+                        unique_screens_number_map[screen],i ==(len(user_screens)-1)
                     )
+                    past_node_ptr = node_ptr
                     node_ptr = node_ptr.add_child(new_node)
+                    node_ptr.add_child(past_node_ptr)
         return env_map
 
     def _push(self, node_name: str) -> None:
@@ -116,48 +151,57 @@ class FintechAppDRLEnv(gym.Env):
     def _pop(self) -> int:
         return self.__past_states.pop()
 
-    def step(self, action: int) -> tuple[list[int], int, int, bool]:
+    def step(self, action: int) -> tuple[list[int], int, bool]:
         self.render()
         if self.__current_node.is_child(self.__number_unique_screen_map[action]):
             retrieved_node = self.__current_node.return_child(
                 self.__number_unique_screen_map[action]
             )
-            if retrieved_node is None:
-                checked_value = NodeType.check_wrong_true(self.__current_node)
-                return self.__past_states, checked_value, checked_value, True
-            elif self.__current_node.has_child():
-                checked_value = NodeType.check_wrong_true(retrieved_node)
-                self._push(retrieved_node)
+            if self.__current_node.has_child():
+                self._push(retrieved_node.name)
                 self.__current_node = retrieved_node
-                return self.__past_states, checked_value, checked_value, False
-            else:
+            elif self.__current_node is None:
                 raise RuntimeError(
                     f"There is no child like that check child {retrieved_node.name}"
                 )
-        return self.__past_states, -1, -1, False
+            if bool(self.__current_node):
+                print(f"finished with {NodeType.retrieve_bool_text(self.__current_node)}")
+                print(f"true finished count: {self.__true_finished_count}")
+                print("-"*20)
+                self.__true_finished_count += 1
+                return self.__past_states, NodeType.check_wrong_true(self.__current_node), True
+            return self.__past_states, self.__current_node.reward, bool(self.__current_node)
+        return self.__past_states, -2, bool(self.__current_node)
 
     def reset(self, seed=None, options=None) -> list[int]:
-        map_start = np.random.choice(list(self.__env_map.keys()))
         self.__past_states = [0] * (self.__memory_len - 1)
+        if seed is None:
+            map_start : str = np.random.choice(list(self._env_map.keys()))
+        else:
+            keys_listed = list(self._env_map.keys())
+            if len(keys_listed) <= seed:
+                raise RuntimeError(f"possible seed 0 to {len(keys_listed)-1}, yours seed is {seed}")
+            map_start = keys_listed[seed]
         self._push(map_start)
+        self.__current_node = self._env_map[map_start]
+        self.render()
         return self.__past_states
 
     def render(self):
         self.__screen.fill("white")
         main_pygame_env = MainEnv(self.__screen)
-        nodes = [node for node in self.__current_node.return_children()] + [
-            self.__current_node
-        ]
+        nodes = self.__current_node.return_children()
         drawable_nodes = [
             DrawableNode(
                 node.name,
                 -1,
-                NodeType.retrieve_node_type(node.color, -1, self.__screen),
+                NodeType.retrieve_node_type(node.color), -1, self.__screen
             )
             for node in nodes
-        ]
-        
+        ] + [ DrawableNode(self.__current_node.name,-1,NodeType.CURRENT,-1,self.__screen)]
+        rendered_text = self.font.render(str(NodeType.retrieve_bool_text(self.__current_node)),False,self.__current_node.color,"white")
         main_pygame_env.draw(drawable_nodes)
+        self.__screen.blit(rendered_text,(50,50))
         pg.display.flip()
         self.__clock.tick(60)
 
