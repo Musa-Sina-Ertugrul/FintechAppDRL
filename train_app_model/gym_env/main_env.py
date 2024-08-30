9289f01e9d425fa318b4c471fb485b3cd88b0f73
Musa-Sina-Ertugrul <yutubems@gmail.com>
2024-08-30 16:22:03 +0300

    Adjusted rewards and Solved several bugs

--- train_app_model/gym_env/main_env.py
+++ train_app_model/gym_env/main_env.py
@@ -11,13 +11,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 class FintechAppDRLEnv(gym.Env):
 
     metadata = {"render_modes": ["human"], "render_fps": 60}
-    
+
     font: pg.font.Font = pg.font.SysFont(pg.font.get_fonts()[0], 44)
 
     def __init__(self, memory_len: int):
         super().__init__()
         pg.init()
-        self.__true_finished_count = 0
+        self.__finished_count = 0
         self.__screen = pg.display.set_mode((1280, 720))
         self.__clock = pg.time.Clock()
         self.__memory_len = memory_len
@@ -38,8 +38,8 @@ class FintechAppDRLEnv(gym.Env):
         df = df.drop("enrolled_date", axis=1)
         df["numscreens"] = df["numscreens"].astype(int)
         df["liked"] = df["liked"].astype(int)
-        df = df[df["numscreens"] > 10]
-        df = df[df["numscreens"] < 50]
+        df = df[df["numscreens"] > 9]
+        df = df[df["numscreens"] < 15]
         df_liked = df[df["liked"] == 1]
         df_not_liked = df[df["liked"] == 0]
         screens_liked = df_liked["screen_list"].to_list()
@@ -60,7 +60,7 @@ class FintechAppDRLEnv(gym.Env):
         number_unique_screen_map = {}
         unique_screens = list(CountVectorizer().fit(unique_screens).vocabulary_)
         for i, screen in zip(count(2), unique_screens):
-            unique_screens_number_map[screen] = i 
+            unique_screens_number_map[screen] = i
             number_unique_screen_map[i] = screen
         unique_screens_number_map["pad_screen"] = 0
         number_unique_screen_map[0] = "pad_screen"
@@ -69,12 +69,12 @@ class FintechAppDRLEnv(gym.Env):
         self.__number_unique_screen_map = number_unique_screen_map
         self.__unique_screens_number_map = unique_screens_number_map
         keys_set = set(unique_screens)
-        for i,user_screens in enumerate(separated_screens_liked.copy()):
-            for j,screen in enumerate(user_screens):
+        for i, user_screens in enumerate(separated_screens_liked.copy()):
+            for j, screen in enumerate(user_screens):
                 if screen not in keys_set:
                     separated_screens_liked[i][j] = "custom_screen"
-        for i,user_screens in enumerate(separated_screens_not_liked.copy()):
-            for j,screen in enumerate(user_screens):
+        for i, user_screens in enumerate(separated_screens_not_liked.copy()):
+            for j, screen in enumerate(user_screens):
                 if screen not in keys_set:
                     separated_screens_not_liked[i][j] = "custom_screen"
         results = []
@@ -103,24 +103,26 @@ class FintechAppDRLEnv(gym.Env):
             for i, screen in enumerate(user_screens):
                 if i == 0 and hasattr(env_map, screen):
                     node_ptr = env_map[screen]
-                elif i == 0 and not hasattr(env_map, screen):
+                elif i == 0 and (not hasattr(env_map, screen)):
                     node_ptr = Node(
                         screen,
-                        -(len(user_screens)-i)/len(user_screens),
+                        0.5,
                         NodeType.TRUE,
-                        unique_screens_number_map[screen],i ==(len(user_screens)-1)
+                        unique_screens_number_map[screen],
+                        i == (len(user_screens) - 1),
                     )
                     env_map[screen] = node_ptr
                 else:
                     new_node = Node(
                         screen,
-                        -(len(user_screens)-i)/len(user_screens),
+                        +1 ,
                         NodeType.TRUE,
-                        unique_screens_number_map[screen],i ==(len(user_screens)-1)
+                        unique_screens_number_map[screen],
+                        i >= (len(user_screens) - 1),
                     )
                     past_node_ptr = node_ptr
                     node_ptr = node_ptr.add_child(new_node)
-                    node_ptr.add_child(past_node_ptr)
+                    # node_ptr.add_child(past_node_ptr)
 
         for user_screens in separated_screens_not_liked:
             past_node_ptr = None
@@ -131,16 +133,18 @@ class FintechAppDRLEnv(gym.Env):
                 elif i == 0 and not hasattr(env_map, screen):
                     node_name = np.random.choice(list(env_map.keys()))
                     node_ptr = env_map[node_name]
-                else:                        
+                else:
                     new_node = Node(
                         screen,
-                        -1,
+                        -1 - ((i + 1) / (len(user_screens))),
                         NodeType.WRONG,
-                        unique_screens_number_map[screen],i ==(len(user_screens)-1)
+                        unique_screens_number_map[screen],
+                        i >= (len(user_screens) - 1),
                     )
                     past_node_ptr = node_ptr
                     node_ptr = node_ptr.add_child(new_node)
-                    node_ptr.add_child(past_node_ptr)
+                    if not NodeType.retrieve_bool(node_ptr):
+                        node_ptr.add_child(past_node_ptr)
         return env_map
 
     def _push(self, node_name: str) -> None:
@@ -151,41 +155,58 @@ class FintechAppDRLEnv(gym.Env):
     def _pop(self) -> int:
         return self.__past_states.pop()
 
-    def step(self, action: int) -> tuple[list[int], int, bool]:
+    def step(self, action: int) -> tuple[list[int], int, bool, bool]:
         self.render()
         if self.__current_node.is_child(self.__number_unique_screen_map[action]):
             retrieved_node = self.__current_node.return_child(
                 self.__number_unique_screen_map[action]
             )
-            if self.__current_node.has_child():
-                self._push(retrieved_node.name)
-                self.__current_node = retrieved_node
-            elif self.__current_node is None:
-                raise RuntimeError(
-                    f"There is no child like that check child {retrieved_node.name}"
-                )
+            self.__current_node = retrieved_node
+            self._push(retrieved_node.name)
             if bool(self.__current_node):
-                print(f"finished with {NodeType.retrieve_bool_text(self.__current_node)}")
-                print(f"true finished count: {self.__true_finished_count}")
-                print("-"*20)
-                self.__true_finished_count += 1
-                return self.__past_states, NodeType.check_wrong_true(self.__current_node), True
-            return self.__past_states, self.__current_node.reward, bool(self.__current_node)
-        return self.__past_states, -2, bool(self.__current_node)
+                return self.__last_step()
+            return (
+                self.__past_states,
+                self.__current_node.reward,
+                False,
+                NodeType.retrieve_bool(self.__current_node),
+            )
+        if bool(self.__current_node):
+            return self.__last_step()
+        return (
+            self.__past_states,
+            -2,
+            bool(self.__current_node),
+            NodeType.retrieve_bool(self.__current_node),
+        )
+
+    def __last_step(self)-> tuple[list[int], int, bool, bool]:
+        print(f"finished with {NodeType.retrieve_bool_text(self.__current_node)}")
+        print(f"finish count: {self.__finished_count}")
+        print("-" * 20)
+        self.__finished_count += 1
+        return (
+            self.__past_states,
+            NodeType.check_wrong_true(self.__current_node),
+            True,
+            NodeType.retrieve_bool(self.__current_node),
+        )
 
     def reset(self, seed=None, options=None) -> list[int]:
         self.__past_states = [0] * (self.__memory_len - 1)
         if seed is None:
-            map_start : str = np.random.choice(list(self._env_map.keys()))
+            map_start: str = np.random.choice(list(self._env_map.keys()))
         else:
             keys_listed = list(self._env_map.keys())
             if len(keys_listed) <= seed:
-                raise RuntimeError(f"possible seed 0 to {len(keys_listed)-1}, yours seed is {seed}")
+                raise RuntimeError(
+                    f"possible seed 0 to {len(keys_listed)-1}, yours seed is {seed}"
+                )
             map_start = keys_listed[seed]
         self._push(map_start)
         self.__current_node = self._env_map[map_start]
         self.render()
-        return self.__past_states
+        return self.__past_states, self.possible_locations_input()
 
     def render(self):
         self.__screen.fill("white")
@@ -195,15 +216,47 @@ class FintechAppDRLEnv(gym.Env):
             DrawableNode(
                 node.name,
                 -1,
-                NodeType.retrieve_node_type(node.color), -1, self.__screen
+                NodeType.retrieve_node_type(node.color),
+                -1,
+                self.__screen,
             )
             for node in nodes
-        ] + [ DrawableNode(self.__current_node.name,-1,NodeType.CURRENT,-1,self.__screen)]
-        rendered_text = self.font.render(str(NodeType.retrieve_bool_text(self.__current_node)),False,self.__current_node.color,"white")
+        ] + [
+            DrawableNode(
+                self.__current_node.name, -1, NodeType.CURRENT, -1, self.__screen
+            )
+        ]
+        rendered_text = self.font.render(
+            str(NodeType.retrieve_bool_text(self.__current_node)),
+            False,
+            self.__current_node.color,
+            "white",
+        )
         main_pygame_env.draw(drawable_nodes)
-        self.__screen.blit(rendered_text,(50,50))
+        self.__screen.blit(rendered_text, (50, 50))
         pg.display.flip()
-        self.__clock.tick(60)
+
+    def get_possible_locations(self) -> list[int]:
+
+        current_possible_nodes = []
+        for child_node in self.__current_node.return_children():
+            current_possible_nodes.append(child_node.name)
+        converted_possible_nodes = []
+        for child_node_name in current_possible_nodes:
+            converted_possible_nodes.append(
+                self.__unique_screens_number_map[child_node_name]
+            )
+        return converted_possible_nodes
+
+    def possible_locations_input(self, input_len: int = 10) -> list[int]:
+        possible_locations = self.get_possible_locations()
+        if input_len < len(possible_locations):
+            raise RuntimeError(
+                f"input array len is {input_len}, possible locations input array len is {len(possible_locations)} »»» input len is smaller"
+            ) from None
+        locations = [0] * input_len
+        locations[: len(possible_locations)] = possible_locations
+        return locations
 
     def close(self):
         pg.quit()
